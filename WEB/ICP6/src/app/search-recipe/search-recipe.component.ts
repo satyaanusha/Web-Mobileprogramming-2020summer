36ccf446f00dc3fbf26f38f0ec54443a55081ba9
satyaanusha <39612435+satyaanusha@users.noreply.github.com>
2020-06-26 09:54:05 -0500

    Update search-recipe.component.ts

--- WEB/ICP6/src/app/search-recipe/search-recipe.component.ts
+++ WEB/ICP6/src/app/search-recipe/search-recipe.component.ts
@@ -1,68 +1,113 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
+// @ts-ignore
 @Component({
-  selector: 'app-search-recipe',
-  templateUrl: './search-recipe.component.html',
-  styleUrls: ['./search-recipe.component.css']
+  selector: 'app-search-restaurant',
+  templateUrl: './search-restaurant.component.html',
+  styleUrls: ['./search-restaurant.component.css']
 })
-export class SearchRecipeComponent implements OnInit {
+export class SearchRestaurantComponent implements OnInit {
+
+  // @ts-ignore
   @ViewChild('recipe') recipes: ElementRef;
+  // @ts-ignore
   @ViewChild('place') places: ElementRef;
   recipeValue: any;
   placeValue: any;
   venueList = [];
   recipeList = [];
-  formattedAddress = [];
+  url = '';
 
   currentLat: any;
   currentLong: any;
   geolocationPosition: any;
 
-  recepieApi = 'https://api.edamam.com/search?q=';
-  recepieAppid = '&app_id=ba18fdc5';
-  recepieKey = '&app_key=588b400e488e39a56644f2a2e8094f12';
 
-
-  constructor(private _http: HttpClient) {
-  }
-
-  ngOnInit() {
-//for getting the users current location
+  constructor(private http: HttpClient) {
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.geolocationPosition = position;
         this.currentLat = position.coords.latitude;
+        console.log(this.currentLat);
         this.currentLong = position.coords.longitude;
+
       });
+
+
   }
-//this method get the locations and we are only storing the label, icons and url and showing it on the html page.
+  getInit() {
+    this.recipeValue = this.recipes.nativeElement.value;
+    if (this.recipeValue === '') {
+      this.recipeValue = 'food';
+    }
+    this.http.get('https://api.foursquare.com/v2/venues/search' +
+      '?client_id=3PPNMTIKJJNDVYPFOBGSHHV2PR5A2P05PYHXDN2MKSKTTBSX' +
+      '&client_secret=0QPHT0F5RS043F4TB4KKPQSHKSAXKE5ZNOYGB5KL2MBDYAG4' +
+      '&v=20160215&limit=5' +
+      '&ll=' + this.currentLat + ',' + this.currentLong +
+      '&query=' + this.recipeValue)
+      .subscribe((data: any) => {
+        for (let i = 0; i < data.response.venues.length; i++) {
+          this.venueList[i] = {
+            name: data.response.venues[i].name,
+            id: data.response.venues[i].id,
+            location: data.response.venues[i].location
+          };
+          console.log(this.venueList[i]);
+
+        }
+
+      });
+
+  }
+
+  ngOnInit() {
+
+
+
+  }
+
   getVenues() {
+    console.log(this.currentLat);
     this.recipeValue = this.recipes.nativeElement.value;
     this.placeValue = this.places.nativeElement.value;
-    //for the recipes we are limiting the results to 5 recipes
+
     if (this.recipeValue !== null) {
-      this._http.get("https://api.edamam.com/search?q=" +this.recipeValue + "&app_id=ba18fdc5&app_key=588b400e488e39a56644f2a2e8094f12&to=5")
-          .subscribe((result: any) => {
-        console.log(result);
-        this.recipeList = Object.keys(result.hits).map(function (k) {
-          const i = result.hits[k].recipe;
-          return {name: i.label, icon: i.image, url: i.url};
+      this.http.get('https://api.edamam.com/search?q=' + this.recipeValue + '&app_id=90345b31&app_key=7884f37e59a7ff7d16ceb275bec553a9&from=0&to=3')
+        .subscribe((data: any) => {
+          for (let i = 0; i < data.hits.length; i++) {
+            this.recipeList[i] = {
+              name: data.hits[i].recipe.label,
+              url: data.hits[i].recipe.url,
+              icon: data.hits[i].recipe.image
+            };
+          }
         });
-
-      });
-
     }
-//for the location search we are limiting the location to 8 results
+
+
+
     if (this.placeValue != null && this.placeValue !== '' && this.recipeValue != null && this.recipeValue !== '') {
-      this._http.get("https://api.foursquare.com/v2/venues/search?client_id=R0DXXKM5BTJMRK3HRXMCRZ3QK3UG3LM1AUDLHDNEBQZRCYTZ" +
-          "&client_secret=IOWJRGIHCVO2UIYKFLPQCRL3WDYIEDX1FJVLET2J3XMRHLHT&v=20180323&limit=8&near=" +this.placeValue).subscribe((result: any) => {
-        console.log(result);
-        this.venueList = Object.keys(result.response.venues).map(function (k) {
-          const i = result.response.venues[k];
-          return {name: i.name, currentLat: '39.0349657', currentLong: '-94.5787524', formattedAddress: i.location.formattedAddress};
+      this.http.get('https://api.foursquare.com/v2/venues/search' +
+        '?client_id=3PPNMTIKJJNDVYPFOBGSHHV2PR5A2P05PYHXDN2MKSKTTBSX' +
+        '&client_secret=0QPHT0F5RS043F4TB4KKPQSHKSAXKE5ZNOYGB5KL2MBDYAG4' +
+        '&v=20160215&limit=5' +
+        '&near=' + this.placeValue +
+        '&query=' + this.recipeValue)
+        .subscribe((data: any) => {
+          for (let i = 0; i < data.response.venues.length; i++) {
+            this.venueList[i] = {
+              name: data.response.venues[i].name,
+              id: data.response.venues[i].id,
+              location: data.response.venues[i].location
+            };
+            console.log(this.venueList[i]);
+
+          }
+
         });
-      });
     }
   }
+
 }
